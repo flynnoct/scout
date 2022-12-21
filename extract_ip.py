4fd75696e05e5bacb99b1d013728c50974bfccc5
flynnoct <me@flynmail.com>
2022-12-21 00:20:37 -0500

    implemented extract_ip python script

--- extract_ip.py
+++ extract_ip.py
@@ -9,4 +9,14 @@ parser.add_argument("-o", "--output", help="Output File", type=str)
 args = parser.parse_args()
 
 with open(args.input, "r") as f:
-    
+    masscan_data = json.load(f)
+
+deduplicate_data = set()
+
+for item in masscan_data:
+    deduplicate_data.add(item["ip"])
+
+with open(args.output, "w") as wf:
+    for ip in deduplicate_data:    
+        wf.write(ip + "\n")
+
